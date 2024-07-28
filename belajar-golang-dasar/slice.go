8a56e6dbad53dc4a82de304450d52e2e2122eac9
abyankamal <kamalmuhammad414@gmail.com>
2024-07-28 16:22:26 +0700

    hari keempat belajar golang

--- belajar-golang-dasar/slice.go
+++ belajar-golang-dasar/slice.go
@@ -15,4 +15,53 @@ func main() {
 	fmt.Println(slice4)
 	slice5 := names[:] // ambil semua nilai
 	fmt.Println(slice5)
+
+	days := [...]string{"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"}
+
+	daySlice1 := days[5:] // Sabtu, Minggu
+	fmt.Println(daySlice1)
+
+	daySlice1[0] = "Sabtu Baru"
+	daySlice1[1] = "Minggu Baru"
+	fmt.Println(daySlice1)
+	fmt.Println(days)
+
+	daySlice2 := append(daySlice1, "Libur Baru")
+	daySlice2[0] = "Sabtu Lama"
+	// daysBaru := [...]string{"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu", "Libur Baru"}
+	fmt.Println(daySlice1)
+	fmt.Println(daySlice2)
+	fmt.Println(days)
+
+	var newSlice []string = make([]string, 2, 5)
+	newSlice[0] = "Eko"
+	newSlice[1] = "Eko"
+	// newSlice[2] = "Eko" // error, harusnya menggunakan append
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Eko")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Budi"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
+
+	iniArray := [...]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
 }
