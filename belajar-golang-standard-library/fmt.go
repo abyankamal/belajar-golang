ff42c8ce4622a680c93b4f846239ef06298ae1c6
abyankamal <kamalmuhammad414@gmail.com>
2024-08-03 11:16:26 +0700

    hari kesepuluh belajar golang

---
+++ belajar-golang-standard-library/fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Golang!")
+
+	firstName := "Abyan"
+	lastName := "Kamal"
+
+	fmt.Printf("Hello %s %s \n", firstName, lastName)
+}
