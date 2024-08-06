bbb1bca5afcd699a3229a49d9a05a8b1124afeed
abyankamal <kamalmuhammad414@gmail.com>
2024-08-07 06:57:24 +0700

    hari ketigabelas belajar golang

--- belajar-golang-unit-test/go.mod
+++ belajar-golang-unit-test/go.mod
@@ -1,3 +1,5 @@
 module belajar-golang-unit-test
 
 go 1.22.5
+
+require github.com/stretchr/testify v1.9.0
--- belajar-golang-unit-test/helper/hello_world_test.go
+++ belajar-golang-unit-test/helper/hello_world_test.go
@@ -1,17 +1,33 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSayHelloAssertion(t *testing.T) {
+    result := SayHello("World")
+    assert.Equal(t, "Hello World", result, "They Should Be Equal")
+    
+    fmt.Println("Test SayHello Assertion Done")
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := SayHello("World")
     if result!= "Hello World" {
         t.Error("Expected 'Hello World', got", result)
     }
+
+    fmt.Println("Test Hello World Done")
 }
 
 func TestHelloEko(t *testing.T) {
 	result := SayHello("Eko")
     if result!= "Hello Eko" {
-        t.Error("Expected 'Hello Eko', got", result)
+        t.Fatal("Expected 'Hello Eko', got", result)
     }
+
+    fmt.Println("Test Hello World Done")
 }
