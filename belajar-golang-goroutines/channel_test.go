238ccac7099e1a10ad0097aefa394391e1e5023c
abyankamal <kamalmuhammad414@gmail.com>
2024-08-09 09:50:05 +0700

    hari kelimabelas belajar golang

---
+++ belajar-golang-goroutines/channel_test.go
@@ -0,0 +1,21 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Eko Kurniawan Khannedy"
+		fmt.Println("Selesai Mengirim Data ke Channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+}
