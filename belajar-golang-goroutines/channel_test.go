ab4510d9f711e0b8c624530448b8cdf1eeb2f3db
abyankamal <kamalmuhammad414@gmail.com>
2024-08-12 09:37:38 +0700

    hari keenambelas belajar golang

--- belajar-golang-goroutines/channel_test.go
+++ belajar-golang-goroutines/channel_test.go
@@ -57,3 +57,23 @@ func TestInOutChannel(t *testing.T) {
 	data := <-channel
 	fmt.Println(data)
 }
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Eko"
+		channel <- "Kurniawan"
+		channel <- "Khannedy"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
