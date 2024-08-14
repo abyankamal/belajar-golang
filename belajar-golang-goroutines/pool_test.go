30af6a126ee4da9edf737a6e425401b0443fa1a6
abyankamal <kamalmuhammad414@gmail.com>
2024-08-14 21:13:52 +0700

    hari kedelapanbelas belajar golang

--- belajar-golang-goroutines/pool_test.go
+++ belajar-golang-goroutines/pool_test.go
@@ -10,18 +10,23 @@ import (
 func TestPool(t *testing.T) {
 	pool := sync.Pool{
 		New: func() interface{} {
-			return "New"
+			s := "New"
+			return &s
 		},
 	}
 
-	pool.Put("EKo")
-	pool.Put("Kurniawan")
-	pool.Put("Khannedy")
+	eko := "Eko"
+    kurniawan := "Kurniawan"
+    khannedy := "Khannedy"
+    pool.Put(&eko)
+    pool.Put(&kurniawan)
+    pool.Put(&khannedy)
+
 
 	for i := 0; i < 10; i++ {
 		go func() {
-			data := pool.Get()
-			fmt.Println(data)
+			data := pool.Get().(*string)
+			fmt.Println(*data)
 			time.Sleep(1 * time.Second)
 			pool.Put(data)
 		}()
